3df84db8f554f901026606f8b85d9ca8012bfc38
Arhi74 <alexey.andryushin@mail.ru>
2023-12-21 19:06:23 +0500

    Созданы скелеты моделей

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.model):
+    pass
+
+
+class Category(models.model):
+    pass
+
+
+class Post(models.model):
+    pass
+
+
+class PostCategory(models.model):
+    pass
+
+
+class Comment(models.model):
+    pass
