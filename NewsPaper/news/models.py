a5557542bfee2a90380b23b54c8ae40e0cf78969
Arhi74 <alexey.andryushin@mail.ru>
2023-12-22 11:44:02 +0500

    Описаны методы всех моделей

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
 
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        post_rating = Post.objects.filter(author=self).aggregate(pr=Coalesce(Sum('rating'), 0))['pr']
+        comment_rating = Comment.objects.filter(user=self.user).aggregate(cr=Coalesce(Sum('rating'), 0))['cr']
+        post_comment_rating = Comment.objects.filter(post__author=self).aggregate(pcr=Coalesce(Sum('rating'), 0))['pcr']
+
+        self.rating = post_rating * 3 + comment_rating + post_comment_rating
+        self.save()
 
 
 class Category(models.Model):
@@ -28,6 +38,14 @@ class Post(models.Model):
     text = models.TextField(default='')
     rating = models.IntegerField(default=0)
 
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -40,3 +58,12 @@ class Comment(models.Model):
     text = models.TextField(default='')
     datetime = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
