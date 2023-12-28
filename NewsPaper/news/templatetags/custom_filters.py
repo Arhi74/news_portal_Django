74ea616b424782459f1c9417c00e2688851fe777
Arhi74 <alexey.andryushin@mail.ru>
2023-12-28 20:17:36 +0500

    Добавлен фильтр нецензурных слов censor Добавлено выравнивание таблицы по центру и растягивание по ширине

---
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -0,0 +1,23 @@
+from django import template
+from .bad_words import bad_words
+
+register = template.Library()
+
+
+@register.filter()
+def censor(value):
+    split_str = value.split()
+    new_split_string = []
+
+    for word in split_str:
+        for bad_word in bad_words:
+            bad_len = len(bad_word)
+            index = word.lower().find(bad_word)
+            if index != -1:
+                print(word, bad_word, index)
+                new_split_string.append(word[:index] + '*' * bad_len + word[index + bad_len:])
+                break
+        else:
+            new_split_string.append(word)
+
+    return ' '.join(new_split_string)
