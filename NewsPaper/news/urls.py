ba64aa469b83de4728f393d0430d19ba01ca028d
Arhi74 <alexey.andryushin@mail.ru>
2024-01-05 19:35:07 +0500

    Добавлены ссылки на добавление, редактирование и удаление новостей\статей. Добавлена ссылка на возврат к списку новостей.

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
    path('news/search', PostSearch.as_view(), name='post_search'),
    path('news/create/', NewsCreate.as_view(), name='news_create'),
    path('news/<int:pk>/update/', NewsUpdate.as_view(), name='news_update'),
-   path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_Delete'),
-   path('articles/create/', ArticlesCreate.as_view(), name='news_create'),
-   path('articles/<int:pk>/update/', ArticlesUpdate.as_view(), name='news_update'),
-   path('articles/<int:pk>/delete/', ArticlesDelete.as_view(), name='news_Delete'),
+   path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
+   path('articles/create/', ArticlesCreate.as_view(), name='articles_create'),
+   path('articles/<int:pk>/update/', ArticlesUpdate.as_view(), name='articles_update'),
+   path('articles/<int:pk>/delete/', ArticlesDelete.as_view(), name='articles_delete'),
 ]
